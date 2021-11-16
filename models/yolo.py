bd7c857eb7cb456d9bd9af354b0cb0d7332aa22a
xialuxi <297509086@qq.com>
2021-11-16 09:48:26 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -285,7 +285,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in [Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
                  BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, RepVGGBlock, SEBlock, Shuffle_Block, conv_bn_relu_maxpool, 
-                 MobileNetV3_Block, MobileNetV2_Block]:
+                 MobileNetV3_Block, MobileNetV2_Block，CBAM]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
